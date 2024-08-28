d408366be29cbc9a0a43b4d1694084fc9ac1021a
TEJADA ARAGON EMILIANO <49380010@ort>
2024-08-28 15:53:21 -0300

    python qr detector done

--- main.py
+++ main.py
@@ -1,20 +1,35 @@
-import numpy as np
-import requests as req
 import cv2
+import time
+import requests as req
+import json
 
-capture=cv2.VideoCapture(0)
-while(capture.isOpened()):
-    ret,frame=capture.read()
-    cv2.imshow("webcam",frame)
-    if(cv2.waitKey(1)==ord("s")):
-        break
-    qrDetector=cv2.QRCodeDetector()
-    data,bbox,rectifiedImage=qrDetector.detectAndDecode(frame)
-    if(len(data)>0):
-        print(data)
-        res=req.post("https://esp32-rfid-api.vercel.app/web/llavero", data)
+def sendRequest(data):
+    payload={
+        "TOKEN":data,
+        "CARRO_ID":7
+    }
+    try:        
+        res=req.put("https://secure-track-db.vercel.app/computers/withdrawal",json=payload, headers={'content-type': 'application/json'})
         if res.status_code==200:
-            print(res.content)
-            break
+            return res.json()
         else:
-            print(res.status_code)
+            return res.status_code
+    except Exception as e:
+        return(e) 
+
+capture = cv2.VideoCapture(0)
+qrDetector = cv2.QRCodeDetector()
+while capture.isOpened():
+    ret, frame = capture.read()
+    cv2.imshow("webcam", frame)
+    if cv2.waitKey(1) == ord("q"):
+        break
+    data, bbox, rectifiedImage = qrDetector.detectAndDecode(frame)
+    if len(data) > 0:
+        print(f"QR Code detected: {data}")
+        print(sendRequest(data))
+        time.sleep(2)
+
+    time.sleep(0.05)
+capture.release()
+cv2.destroyAllWindows()
